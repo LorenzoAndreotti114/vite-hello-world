abcc434d82c2280c4aaf9d0fcee1e52fd5a21043
LorenzoAndreotti114 <lorenzoandr123@gmail.com>
2024-05-02 10:27:02 +0200

    final project

---
+++ src/components/appTitle.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="banner">
+        <h1>
+            la mia prima app con vite
+        </h1>
+    </div>
+</template>
+
+<style>
+    .banner {
+        display: flex;
+        justify-content: center;
+        background-color: aqua;
+        border: 1px solid black;
+        font-size: 2vw;
+    }
+</style>
